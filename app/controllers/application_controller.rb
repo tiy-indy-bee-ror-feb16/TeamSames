289cb88ead5db60683e8482d6a6825416a195c04
J-Henry <JH@JH.local>
2016-04-07 19:27:43 -0400

    Add user helpers: current_user, require_user, disallow_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+    private
+
+    def current_user
+      if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id])
+      end
+    end
+
+    def require_user
+      unless current_user
+        flash[:danger] = "Log in for that, please!"
+        redirect_to :root
+      end
+    end
+
+    def disallow_user
+      if current_user
+        flash[:info] = "You're already logged in. Sparkle on!"
+        redirect_to :root
+      end
+    end
 end
