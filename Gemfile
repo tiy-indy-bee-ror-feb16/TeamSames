14c493248f1f081a6d79ce7c820ca42b88bdc537
Seth Baughman <sbaughman1@me.com>
2016-04-07 16:25:37 -0400

    add kaminari-bootstrap gem. styling

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'kaminari'
 gem 'bootstrap-sass'
 gem 'rails_12factor', group: :production
 gem 'faker'
+gem 'bootstrap-kaminari-views'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
--- app/models/gleet.rb
+++ app/models/gleet.rb
@@ -1,4 +1,5 @@
 class Gleet < ActiveRecord::Base
+  paginates_per 10
   belongs_to :user
   validates :body, presence: true
   validates :body, length: { maximum: 170, too_long: "%{count} characters is the maximum allowed" }
