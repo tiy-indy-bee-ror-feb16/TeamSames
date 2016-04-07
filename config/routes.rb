2ad8c6dcba211c714064485704eb63a66f935955
J-Henry <JH@JH.local>
2016-04-07 18:48:40 -0400

    Try user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :gleets
   resources :users
-  root 'gleets#index'
-  get '/signup' => 'users#new'
+  root   'gleets#index'
+  get    '/signup' => 'users#new'
+  get    '/login' => 'sessions#new', as: :login
+  post   '/login' => 'sessions#create', as: :login_create
+  delete '/logout' => 'sessions#destroy'
 end
