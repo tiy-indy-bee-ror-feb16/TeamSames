3a059f09e22de9f9108f560ac5dce8fc6175cf21
Seth Baughman <sbaughman1@me.com>
2016-04-07 19:17:11 -0400

    add gleet destroy method

--- app/controllers/gleets_controller.rb
+++ app/controllers/gleets_controller.rb
@@ -13,11 +13,17 @@ class GleetsController < ApplicationController
     @gleet = Gleet.new(gleet_params)
     @gleet.user_id = params[:user_id]
     if @gleet.save
-      flash[:success] = "New Gleet created!"
+      flash[:success] = "A new Gleet shines in the sun!"
       redirect_to :root
     else
       render :new
     end
+  end
+
+  def destroy
+    Gleet.find(params[:id]).destroy
+    flash[:success] = "That Gleet has shined its last"
+    redirect_to :root
   end
 
   private
