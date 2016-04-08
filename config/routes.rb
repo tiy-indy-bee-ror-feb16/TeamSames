61ba49383bc8876e03aa5fc387bd1c64c6d44bed
J-Henry <JH@JH.local>
2016-04-08 13:29:59 -0400

    Add Follow seeding

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   resources :users do
     resources :gleets
   end
-  root 'gleets#index'
-  get '/signup' => 'users#new'
+  root   'gleets#index'
+  get    '/signup' => 'users#new'
   get    '/login' => 'sessions#new', as: :login
   post   '/login' => 'sessions#create', as: :login_create
   delete '/logout' => 'sessions#destroy'
-
 end
