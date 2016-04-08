127831098183b7806d442379c7d77965adf144a3
J-Henry <JH@JH.local>
2016-04-08 15:49:42 -0400

    Add btn id styles

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   root   'gleets#index'
   get    '/signup' => 'users#new'
-  get    '/login' => 'sessions#new', as: :login
-  post   '/login' => 'sessions#create', as: :login_create
+  get    '/login'  =>  'sessions#new', as: :login
+  post   '/login' =>  'sessions#create', as: :login_create
   delete '/logout' => 'sessions#destroy'
 end
