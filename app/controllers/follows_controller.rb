dace605e80ee79bb6f288e6077dd479993ce970f
Seth Baughman <sbaughman1@me.com>
2016-04-08 15:37:09 -0400

    redefine routes

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,16 +1,16 @@
 class FollowsController < ApplicationController
   before_action :require_user
 
-  def show
-    @user = User.find(params[:user_id])
-    current_user.unfollow!(@user)
-    redirect_to @user
-  end
-
   def new
     @user = User.find(params[:user_id])
     current_user.follow!(@user)
     redirect_to @user
   end
-  
+
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow!(@user)
+    redirect_to @user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :gleets
-    resources :follows
   end
+
   root   'gleets#index'
   get    '/signup' => 'users#new'
   get    '/login' => 'sessions#new', as: :login
   post   '/login' => 'sessions#create', as: :login_create
   delete '/logout' => 'sessions#destroy'
+  get    '/users/:user_id/follow' => 'follows#new', as: :follow
+  get    '/users/:user_id/unfollow' => 'follows#destroy', as: :unfollow
+  
 end
