59da74e7f357fe11d7aebfce2cc96f44128eefa7
J-Henry <JH@JH.local>
2016-04-09 15:36:12 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6, too_short: "Password must be at least 6 characters" }
+  before_save { self.email = email.downcase }
+  validates :email, presence: true,
+                    uniqueness: true,
+                    case_sensitive: false,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "Must be a valid email" }
+  validates :password, presence: true,
+                       length: { minimum: 6, too_short: "Password must be at least 6 characters" }
   has_many :gleets
   acts_as_follower
   acts_as_followable
