ce5acb41131af977cd3dc98636fd66806949ffda
J-Henry <JH@JH.local>
2016-04-09 21:24:35 -0400

    Add global gleets path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get    '/login'  => 'sessions#new', as: :login
   post   '/login'  => 'sessions#create', as: :login_create
   delete '/logout' => 'sessions#destroy'
+  get    '/fresh-glitter'           => 'gleets#index', as: :glitter_index
   get    '/users/:user_id/follow'   => 'follows#new', as: :follow
   delete '/users/:user_id/unfollow' => 'follows#destroy', as: :unfollow
   get    '/users/:user_id/block'    => 'blockings#new', as: :block
