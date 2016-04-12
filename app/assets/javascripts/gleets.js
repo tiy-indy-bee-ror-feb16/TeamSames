2d215e73c06e6914998962bbe513434806865e06
Seth Baughman <sbaughman1@me.com>
2016-04-12 17:48:32 -0400

    add javascript to convert times to local time

--- app/assets/javascripts/gleets.js
+++ app/assets/javascripts/gleets.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
       ev.preventDefault();
     }
   });
+  convert_to_local_time();
 });
 
 function count_characters() {
@@ -27,4 +28,28 @@ function validate_length(characters) {
 
 function output_character_count(characters) {
   $('#counter').html("<p>Gleet length: " + characters + "</p>");
+}
+
+function convert_to_local_time() {
+  $('.gleet-time').each(function() {
+    var el = $(this);
+    var utc_time = el.html();
+    var local_time = new Date(utc_time);
+    var hour = local_time.getHours();
+    var unpadded_minutes = local_time.getMinutes().toString();
+    var minutes = zero_pad_minutes(unpadded_minutes);
+    var month = local_time.getMonth();
+    var day = local_time.getDay();
+    var year = local_time.getFullYear();
+    el.html("gleeted on " + month + "/" + day + "/" + year + " at " + hour + ":" + minutes);
+  });
+}
+
+function zero_pad_minutes(min) {
+  console.log(min.length);
+  if (min.length === 1) {
+    return "0" + min;
+  } else {
+    return min;
+  }
 }
